b54369be02c46de353edcace4fc365359f3d8e5d
Norbert Szulc <norbert@not7cd.net>
2018-04-12 23:00:21 +0200

    WTF

---
+++ wtf/w0.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int i = 1;
+    i++;
+    i++;
+    i++;
+    i++;
+    i++;
+    i++;
+    i++;
+    printf("%i\n", i);
+    return 0;
+}
